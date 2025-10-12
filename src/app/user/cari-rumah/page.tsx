a2e532182b60b30f2fb04e417210c38cfdf56eda
ceciliamelva <ceciliamelvaa@gmail.com>
2025-10-12 16:53:40 +0700

    rename folder

--- src/app/user/cari-rumah/page.tsx
+++ src/app/user/cari-rumah/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useMemo } from 'react';
-import Header from '../components/Header';
+import Header from '../../components/Header';
 
 // Dummy property data
 const dummyProperties = [
